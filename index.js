2acdab980f26529db8668db128228a17af03985d
santiago chinellato <santiagochinellato@gmail.com>
2022-07-21 21:29:26 -0300

    se configuaron algunas secciones en mobile

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-$(".carousel").carousel({
-  interval: false,
+$(document).ready(function () {
+  $(".carouselResultados").slick({
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "30%",
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "20%",
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 460,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "15%",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 });
