ee1cf50561ef82c5a676801d936136b66190599a
santiago chinellato <santiagochinellato@gmail.com>
2022-08-05 19:23:18 -0300

    se ultimaron detalles de linkeo

--- index.js
+++ index.js
@@ -37,4 +37,43 @@ $(document).ready(function () {
   });
 });
 
+$(document).ready(function () {
+  $(".carouselServicios").slick({
+    centerMode: true,
+    // centerPadding: "60px",
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    // responsive: [
+    //   {
+    //     breakpoint: 768,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: "30%",
+    //       slidesToShow: 1,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 576,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: "20%",
+    //       slidesToShow: 1,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 460,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: "15%",
+    //       slidesToShow: 1,
+    //     },
+    //   },
+    // ],
+  });
+});
+
 AOS.init();
